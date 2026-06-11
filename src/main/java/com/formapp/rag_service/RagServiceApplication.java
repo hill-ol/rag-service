c0c1ffbefcbb274fca8724384596c7b07b3144d7
Olivia Hill <hill.ol@northeastern.edu>
2026-06-10 23:03:41 -0400

    fix: remove pgvector autoconfigure exclusion

--- src/main/java/com/formapp/rag_service/RagServiceApplication.java
+++ src/main/java/com/formapp/rag_service/RagServiceApplication.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-		org.springframework.ai.vectorstore.pgvector.autoconfigure.PgVectorStoreAutoConfiguration.class
-})
+@SpringBootApplication
 public class RagServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RagServiceApplication.class, args);
